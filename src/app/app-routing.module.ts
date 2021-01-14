66b47dad4dfa8981b45662bdbb52f6e9a57ee0d0
rushisuper <rjavalgekar@gmail.com>
2021-01-14 14:24:40 +0530

    Added about us compenent, exam ui component and implemented menu bar and userDashboard

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { Component, NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
+import { ExamUIComponent } from './exam-ui/exam-ui.component';
+import { HomeComponent } from './home/home.component';
+import { MenuBarComponent } from './menu-bar/menu-bar.component';
+import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
+import { UserLoginComponent } from './user-login/user-login.component';
+import { UserRegistrationComponent } from './user-registration/user-registration.component';
+
+const routes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'user_registration' ,component:UserRegistrationComponent},
+  {path:'user_login' , component:UserLoginComponent},
+  {path:'admin_login' ,component:AdminLoginComponent},
+  {path:'exam_ui',component:ExamUIComponent},
+  {path:'userDashboard',component:UserDashboardComponent},
+  {path:'userNavBar',component:MenuBarComponent},
+  {path:'aboutUs',component:AboutUsComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserRegistrationComponent } from './user-registration/user-registration.component';
+import { UserLoginComponent } from './user-login/user-login.component';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
+import { HomeComponent } from './home/home.component';
+import { ExamUIComponent } from './exam-ui/exam-ui.component';
+import { MenuBarComponent } from './menu-bar/menu-bar.component';
+import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+// import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserRegistrationComponent,
+    UserLoginComponent,
+    AdminLoginComponent,
+    HomeComponent,
+    ExamUIComponent,
+    MenuBarComponent,
+    UserDashboardComponent,
+    AboutUsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
